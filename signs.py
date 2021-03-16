20440ab7cdeaf012f210d08eb96d8e7e156e57e0
Cecilia Karlsson <65887914+tossc@users.noreply.github.com>
2021-03-16 15:31:17 +0100

    added signs for finish and y1
    in signs.py we added signs for finish and Y1, and we took care of what happens
    if the result is only a constant
    
    created new file that calculates the difference in signs from the isotopy
    that takes a straight flow line and returns a tree with an end and a Y1
    
    saved the results in relations_from_eY1isotopy. Not everything is consistent
    with the calculations in the long article, must check.
    
    Do:
    Recalculate the intersection signs for Y1end-isotopy
    Consider the special case when n=1

--- signs.py
+++ signs.py
@@ -6,7 +6,7 @@ from sympy import *
 
 
 #the variables we need
-bmm1,bm1,bmm2,bm2,k1,k2,k,n,mup,mup1,mup2,e1,e2,fp1,fp2 = symbols('bbmm1,bm1,bmm2,bm2,k1,k2,k,n,mup,mup1,mup2,e1,e2,fp1,fp2')
+bmm1,bm1,bmm2,bm2,k1,k2,k,n,mup,mup1,mup2,e1,e2,fp1,fp2,n_switch,mua,wua,bmm = symbols('bbmm1,bm1,bmm2,bm2,k1,k2,k,n,mup,mup1,mup2,e1,e2,fp1,fp2,n_switch,mua,wua,bmm')
 	
 
 #the sign for n-valent negative punctures of type 1. If some of the variables are undetermined, they can be passed as a symbol equal to the symbol in the expression
@@ -32,15 +32,19 @@ def p2typ1_mod2(bmm1_val, bm1_val, k1_val, mup_val, mup1_val):
     expr = bmm1 + bm1 +k1+(n + mup)*(bm1 + 1 + mup1)
     expr_eval = expr.subs([(bmm1,bmm1_val), (bm1, bm1_val), (k1,k1_val), (mup, mup_val), (mup1, mup1_val)])
 
-    return expr_eval.as_poly(domain = 'GF(2)')
-
+    if expr_eval.is_constant():
+        return expr_eval%2
+    else:
+        return trunc(expr_eval,2)
 
 def p2typ2_mod2(bmm1_val, bm1_val, k1_val, e1_val, mup_val):
     expr = bmm1 + bm1 +k1+ e1 + bm1*(n + mup + 1)
     expr_eval = expr.subs([(bmm1,bmm1_val), (bm1, bm1_val), (k1,k1_val), (e1, e1_val),(mup, mup_val)])
     
-    return expr_eval.as_poly(domain = 'GF(2)')
-
+    if expr_eval.is_constant():
+        return expr_eval%2
+    else:
+        return trunc(expr_eval,2)
 
 def Y0_mod2(bmm1_val, bm1_val, bmm2_val, bm2_val, e1_val, e2_val, n_val, fp2_val, k2_val, mup2_val, k_val, k1_val, fp1_val, mup1_val):
 
@@ -50,4 +54,60 @@ def Y0_mod2(bmm1_val, bm1_val, bmm2_val, bm2_val, e1_val, e2_val, n_val, fp2_val
     expr_eval = expr.subs([(bmm1,bmm1_val), (bm1, bm1_val), (bmm2,bmm2_val), (bm2, bm2_val), (e1, e1_val), (e2, e2_val), (n, n_val), (k1,k1_val), (k2,k2_val), (k,k_val), (fp1, fp1_val), (fp2, fp2_val), (mup1, mup1_val), (mup2, mup2_val)])
 
  
-    return expr_eval.as_poly(domain = 'GF(2)')
+    if expr_eval.is_constant():
+        return expr_eval%2
+    else:
+        return trunc(expr_eval,2)
+
+def Y1_mod2(bmm1_val, bm1_val, bmm2_val, bm2_val, e1_val, e2_val, n_val, fp2_val, k2_val, mup2_val, k_val, k1_val, fp1_val, mup1_val):
+
+    expr = bmm1 + bmm2 + bm1 + bm2 + e1*(bm2 + e2 + 1) + n*fp2 + k2 + bm1*(mup2 + fp2 + n + 1) + k + k1 + (n+fp1+ mup1)*(1 + mup2 + bm2) + fp1 + fp2 + 1
+
+
+    expr_eval = expr.subs([(bmm1,bmm1_val), (bm1, bm1_val), (bmm2,bmm2_val), (bm2, bm2_val), (e1, e1_val), (e2, e2_val), (n, n_val), (k1,k1_val), (k2,k2_val), (k,k_val), (fp1, fp1_val), (fp2, fp2_val), (mup1, mup1_val), (mup2, mup2_val)])
+
+ 
+    if expr_eval.is_constant():
+        return expr_eval%2
+    else:
+        return trunc(expr_eval,2)
+
+
+#for the last gluing, where we simplify the formulas a bit and assume that the signs for the switches adjacent to the positive formula is calculated somewhere else
+
+#case 1, when we do not have any conformal variations
+def pos_noconf_mod2(n_switch_val, n_val, wua_val, mua_val):
+
+    expr = (n_switch -1)*n_switch/2*(n+1) + 1 + n_switch*(n*wua + wua) + (1 + wua)*(n + mua)
+
+    expr_eval = expr.subs([(n_switch,n_switch_val), (n,n_val), (wua,wua_val), (mua,mua_val)])
+
+    if expr_eval.is_constant():
+        return expr_eval%2
+    else:
+        return trunc(expr_eval,2)
+
+#case 2, when we have a 1-valent positive puncture and conformal variations
+def pos_1val_mod2(n_switch_val, n_val, wua_val, mua_val, bmm_val, bm1_val, k1_val):
+
+    expr = (n_switch -1)*n_switch/2*(n+1) + bmm + 1 + n_switch*(n*wua + wua +1) +  wua*(n + mua + bm1 + 1) + (n + mua)*(bm1 + 1) + n + k1
+
+    expr_eval = expr.subs([(n_switch,n_switch_val), (n,n_val), (wua,wua_val), (mua,mua_val), (bmm,bmm_val), (bm1,bm1_val), (k1,k1_val)])
+
+    if expr_eval.is_constant():
+        return expr_eval%2
+    else:
+        return trunc(expr_eval,2)
+
+
+#case 3, when we have a 2-valent positive puncture 
+def pos_2val_mod2(n_val, wua_val, mua_val, bm1_val, k1_val, mup1_val, bm2_val, e1_val, e2_val, k2_val, bmm1_val, bmm2_val):
+
+    expr = mup1*(bm1 + bm2 + mua) + bm1*(n+1) + bm2*(n + mua) + e1*(bm2 + e2) + k1 + k2+ bmm1 + bmm2+ bm2
+
+    expr_eval = expr.subs([(n,n_val), (wua,wua_val), (mua,mua_val),  (bm1,bm1_val), (k1,k1_val), (mup1, mup1_val), (bm2,bm2_val), (e1, e1_val), (e2,e2_val), (k2,k2_val), (bmm1,bmm1_val), (bmm2, bmm2_val)])
+
+    if expr_eval.is_constant():
+        return expr_eval%2
+    else:
+        return trunc(expr_eval,2)
---
+++ y1ecalc.py
@@ -0,0 +1,91 @@
+import sympy, signs
+
+'''calculates the sign difference between trees with one positive puncture a and one negative puncture b, and the two different trees we get after a particular Legendrian isotopy, and these trees have in addition one end and one Y1-vertex.'''
+
+from sympy import *
+
+#unknown variables
+n, wua_dim, wsb_dim, a, b = symbols('n,wua_dim,wsb_dim,a,b')
+
+#sign for the tree with only two punctures
+def straight():
+    step_1 = signs.pos_noconf_mod2(0,n, wua_dim, a)
+
+    return step_1
+
+#sign for the tree with the end as b1, b as b2
+def tree_II():
+    step_1 = signs.Y1_mod2(0,0,0,0,1,0,n,n-wua_dim-1,n-wua_dim-1,b,n-wua_dim,n,n,1)
+    step_2 = signs.pos_1val_mod2(0,n,wua_dim,a,1,1,n-wua_dim)
+
+    res = (step_1 + step_2).subs(n**2,n)
+    
+    if res.is_constant():
+        return res%2
+    else:
+        return trunc(res,2)
+
+
+
+#sign for the tree with b as b2, end as b1
+def tree_III():
+    step_1 = signs.Y1_mod2(0,0,0,0,0,1,n,n,n,1,n-wua_dim,n-wua_dim-1,n-wua_dim-1,b)
+    step_2 = signs.pos_1val_mod2(0,n,wua_dim,a,1,1,n-wua_dim)
+
+    res = (step_1 + step_2).subs(n**2,n)
+    
+    if res.is_constant():
+        return res%2
+    else:
+        return trunc(res,2)
+
+
+#now we calculate the difference if we also include the sigmas and the intersection and end signs. We return expressions depending on the parity of the Maslov indicies of the punctures
+
+s_Y1 = symbols('s_Y1')
+
+
+
+def tot_diff_01():
+    ''' difference between the unperturbed tree and the tree with the end as first puncture'''
+    nu_end = 1
+    intersect_diff = n*(wua_dim + 1) + 1
+    sigma_diff = s_Y1(1,b)
+
+    tot = straight() + tree_II() + nu_end + intersect_diff + sigma_diff
+
+    tot1 = tot.subs([(b,a), (n**2,n)])
+
+    if tot1.is_constant():
+        return tot1%2
+    else:
+        return trunc(tot1,2)
+
+
+def tot_diff_02():
+    ''' difference between the unperturbed tree and the tree with the end as second puncture'''
+    nu_end = 0
+    intersect_diff =  1
+    sigma_diff = s_Y1(b,1)
+
+    tot = straight() + tree_III() + nu_end + intersect_diff + sigma_diff
+
+    tot1 = tot.subs([(b,a), (n**2,n)])
+
+    if tot1.is_constant():
+        return tot1%2
+    else:
+        return trunc(tot1,2)
+
+#Now we see what different relation we get by varying the value of the parity of mu(a)
+
+def sigma_diff_from_01():
+    for s in (0,1):
+        p = tot_diff_01().subs(a,s)
+        print(s,trunc(p,2))
+        
+
+def sigma_diff_from_02():
+    for s in (0,1):
+        p = tot_diff_02().subs(a,s)
+        print(s,trunc(p,2))
